08d2ca64985c1062fd011e78ec840459a40ad4f4
arithmetic1728 <liusijun1985@gmail.com>
2021-06-15 18:08:06 -0700

    update

--- sample.py
+++ sample.py
@@ -8,13 +8,7 @@ cert = b"""<fill_in_PEM_cert>"""
 
 key = b"""<fill_in_PEM_private_key>"""
 
-key_info = {
-    "engine_id": b"pkcs11",
-    "pin": b"mynewpin",
-    "key_id": b"1111",
-    "so_path": b"/usr/lib/x86_64-linux-gnu/engines-1.1/pkcs11.so",
-    "module_path": b"/usr/local/lib/softhsm/libsofthsm2.so"
-}
+hsm_key = b"engine:pkcs11:1111:mynewpin"
 
 project = "sijunliu-dca-test"
 
@@ -22,7 +16,7 @@ def my_cert_callback():
     return cert, key
 
 def my_cert_callback_hsm():
-    return cert, key_info
+    return cert, hsm_key
 
 def run_sample(client_cert_callback):
     authed_session = AuthorizedSession(credentials)
