cc38607349c1d44a287577af964ec0e7d96940ed
arithmetic1728 <liusijun1985@gmail.com>
2021-08-18 11:19:51 -0700

    update write cert to hsm sh

--- write_cert_key_to_hsm.sh
+++ write_cert_key_to_hsm.sh
@@ -1,10 +1,14 @@
 rm -rf tokens/*
-rm *.der
+rm -f *.der
+rm -f new-ec*
+rm -f rsa*
 
+echo "==== initializing softHSM =========================="
 pkcs11-tool --module /usr/local/lib/softhsm/libsofthsm2.so --slot-index=0 --init-token --label="token1" --so-pin="123456"
 pkcs11-tool --module /usr/local/lib/softhsm/libsofthsm2.so  --label="token1" --init-pin --so-pin "123456" --pin mynewpin
 
 # For EC keys
+echo "==== writing existing EC keys =========================="
 openssl ec -inform PEM -outform DER -in key.pem -out key.der
 openssl x509 -inform PEM -outform DER -in cert.pem -out cert.der
 
@@ -16,6 +20,9 @@ pkcs11-tool  --module /usr/local/lib/softhsm/libsofthsm2.so --pin mynewpin \
 
 
 # For self generated EC keys
+echo "==== creating and writing new EC keys =========================="
+openssl ecparam -name secp521r1 -genkey -param_enc explicit -out new-ec-key.pem
+openssl req -new -x509 -key new-ec-key.pem -out new-ec-cert.pem -days 730
 
 openssl ec -inform PEM -outform DER -in new-ec-key.pem -out new-ec-key.der
 openssl x509 -inform PEM -outform DER -in new-ec-cert.pem -out new-ec-cert.der
@@ -27,6 +34,8 @@ pkcs11-tool  --module /usr/local/lib/softhsm/libsofthsm2.so --pin mynewpin \
    --write-object new-ec-cert.der --type cert --id 3333 --label neweckey --slot-index 0
 
 # For self generated RSA keys
+echo "==== creating and writing new RSA keys =========================="
+openssl req -nodes -x509 -newkey rsa:4096 -keyout rsakey.pem -out rsacert.pem -days 3650
 
 openssl rsa -inform PEM -outform DER -in rsakey.pem -out rsakey.der
 openssl x509 -inform PEM -outform DER -in rsacert.pem -out rsacert.der
